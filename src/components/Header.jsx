8b9db49f6269eae3b5326213624615040726780d
Shivendra Singh <shivendrasingh@Shivendras-MacBook-Air.local>
2024-06-03 18:26:51 +0530

    Added utils

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,25 @@
-import { useCallback, useContext } from "react";
-import logoImg from "../assets/logo.jpg";
-import Button from "../UI/Button.jsx";
-import CartContext from "../store/CreateContex.jsx";
+
+import { useContext } from 'react';
+
+import Button from '../UI/Button.jsx';
+import logoImg from '../assets/logo.jpg';
+import CartContext from '../store/CartContext.jsx';
+
 export default function Header() {
   const cartCtx = useContext(CartContext);
-  const totalCartitems = cartCtx.items.reduce((totalNumberOfItems , items) => {
-    return totalNumberOfItems + items.quantity
-  }
-    , 0);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
   return (
     <header id="main-header">
       <div id="title">
-        <img src={logoImg} alt="" />
-        <h1>Hungry Hackers</h1>
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
       </div>
       <nav>
-        <Button textOnly>Cart({totalCartitems})</Button>
+        <Button textOnly>Cart ({totalCartItems})</Button>
       </nav>
     </header>
   );
--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,25 +1,31 @@
-import { useContext } from "react";
-import Button from "../UI/Button.jsx";
-import CartContext from "../store/CreateContex.jsx";
-export default function MealItem({ meal }) {
-const cartCtx = useContext( CartContext);
+import { useContext } from 'react';
 
-  function handelAddMealToCart() {
-    cartCtx.addItem(meal)
+import { currencyFormatter } from '../utils/formatting.js';
+import Button from '../UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
+
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
   }
+
   return (
     <li className="meal-item">
       <article>
-        <img src={`http://localhost:3000/${meal.image}`} alt="" />
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
-          <h3>{meal.header}</h3>
-          <p className="meal-item-price">${meal.price}</p>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
-        <div className="meal-item-action">
-          <Button onClick={handelAddMealToCart}>Add to Cart</Button>
-        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>Add to Cart</Button>
+        </p>
       </article>
-    </li>   
+    </li>
   );
 }
